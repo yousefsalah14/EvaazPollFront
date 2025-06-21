7846e38c13de25c600f21a819cd57ae63f379243
Yousef Salah <yousefsalah1242003@gmail.com>
2025-06-21 18:05:35 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
-import Login from './pages/Login/Login'
-import Schools from './pages/Schools/Schools'
-import { AuthProvider } from './contexts/AuthContext'
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import Home from './pages/Home/Home'
+import Form from './pages/Form/Form'
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route 
-            path="/schools" 
-            element={
-              <ProtectedRoute>
-                <Schools />
-              </ProtectedRoute>
-            } 
-          />
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
